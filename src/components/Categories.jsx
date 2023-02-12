81dce75a68a2205a44a660f060c24ac2bef392f8
Evgenii <naterid@yandex.ru>
2023-02-12 23:38:31 +0700

    добавляет рендер категории

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,27 @@
+import React from "react";
 function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  <li onClick={() => onClickCategory(0)} className={activeIndex === 0 ? "active" : ""}>
+    все
+  </li>;
+
+  const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((value, index) => {
+          return (
+            <li onClick={() => onClickCategory(index)} className={activeIndex === index ? "active" : ""}>
+              {value}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
