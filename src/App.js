02ef1caaf4123ce6003a7d8021e52b6d23a2bf7a
Evgenii <naterid@yandex.ru>
2023-02-15 21:21:46 +0700

    Перенес данные на сервер и получаю данные с него по запросу

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React from "react";
+
 import "./scss/app.scss";
 
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
-import Pizzas from "./assets/pizzas.json";
 
 function App() {
+  const [items, setItems] = React.useState([]);
+  React.useEffect(() => {
+    fetch("https://63ece635be929df00cb4a1e8.mockapi.io/items")
+      .then((response) => response.json())
+      .then((items) => setItems(items));
+  }, []); //! сделай запрос на сервер когда комоненты не отрендерены, когда отрендерены не делай, пустой = не отрендерен, не = пустой отрендерен.
+
   return (
     <div className="wrapper">
       <Header />
@@ -18,7 +26,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {Pizzas.map((obj) => {
+            {items.map((obj) => {
               return <PizzaBlock key={obj.id} {...obj} />;
             })}
           </div>
