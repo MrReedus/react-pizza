7ada86a06a5c8157cf7a1b093cc260e14a0f5f0b
Evgenii <naterid@yandex.ru>
2023-02-15 22:52:03 +0700

    добавляет рендер скелетона если страница загружается

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 import Skeleton from "./components/PizzaBlock/Skeleton";
+
 function App() {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
   React.useEffect(() => {
     fetch("https://63ece635be929df00cb4a1e8.mockapi.io/items") //! Получаем данные с сервера
       .then((response) => response.json()) //! преобразуем данные в JSON
-      .then((items) => setItems(items)); //! рендерим данные
+      .then((items) => {
+        setItems(items);
+        setIsLoading(false);
+      }); //! рендерим данные
   }, []); //! сделай запрос на сервер когда комоненты не отрендерены, когда отрендерены не делай, пустой = не отрендерен, не = пустой отрендерен.
 
   return (
@@ -26,9 +32,10 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map((obj) => {
-              return <Skeleton key={obj.id} {...obj} />;
-            })}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+            {/* {items.map((obj) => (isLoading ? <Skeleton /> : <PizzaBlock key={obj.id} {...obj} />))} */}
           </div>
         </div>
       </div>
